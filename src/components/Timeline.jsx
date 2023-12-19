08893f61a1a8632a043f3113036bef575b2ac240
MANI :) <111214511+Manikantan-S@users.noreply.github.com>
2023-12-20 01:01:28 +0530

    Update Timeline.jsx

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,88 +1,49 @@
-import React, { useState, useEffect } from 'react';
-
-const Circle = ({ index, isVisible, onMouseEnter }) => {
-  const backgroundImages = [
-    'url("/images/agrimorning.jpg")',
-    'url("/images/agrievening.jpg")',
-  ];
-
-  const [backgroundIndex, setBackgroundIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setBackgroundIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div
-      className={`w-8 h-8 rounded-full bg-blue-500 mx-20 ${
-        isVisible ? 'opacity-100' : 'opacity-0'
-      } transition-opacity duration-1000 relative`}
-      onMouseEnter={() => onMouseEnter(index)}
-      style={{
-        backgroundImage: backgroundImages[backgroundIndex],
-      }}
-    >
-      {isVisible && (
-        <div
-          className="absolute bg-blue-500 text-white py-1 px-2 rounded text-xs"
-          style={{
-            top: '-80px',
-            left: '50%',
-            transform: 'translateX(-50%)',
-          }}
-        >
-          Step {index + 1}
-        </div>
-      )}
-    </div>
-  );
-};
+import React, { useState } from 'react';
+import fireImage from '../components/images/fire.jpg'; // Import the image
 
 const Timeline = () => {
-  const [hoveredIndex, setHoveredIndex] = useState(0);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [output, setOutput] = useState('');
 
-  const handleHover = (index) => {
-    setHoveredIndex(index);
+  const handleSave = () => {
+    // Simulate an asynchronous operation, like making an API call
+    setLoading(true);
+
+    setTimeout(() => {
+      setOutput(`Dear user ${phoneNumber}, you will be intimated before disaster via SMS.`);
+      setLoading(false);
+    }, 2000); // Simulating a 2-second delay
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="relative">
-        <div
-          className="w-full h-0.5 bg-blue-500 absolute top-1/2 transform -translate-y-1/2"
-        ></div>
-        <div className="flex relative">
-          {[0, 1, 2, 3, 4].map((index) => (
-            <Circle
-              key={index}
-              index={index}
-              isVisible={index === hoveredIndex}
-              onMouseEnter={handleHover}
-            />
-          ))}
-        </div>
+    <div className="container mx-auto p-8 bg-cover bg-center h-3/4" style={{ backgroundImage: `url(${fireImage})` }}>
+      <h1 className="text-5xl font-bold leading-snug mb-4 text-red-500">
+        <span className="text-primary">ALERT SYSTEM</span>
+      </h1>
+      <div className="text-white">
+        {/* Updated input field with increased length */}
+        <input
+          type="text"
+          id="phoneNumber"
+          placeholder="Enter your phone number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          className="p-3 mb-2 rounded-full w-3/4" // Adjusted width to 75% of the container
+        />
+        <button onClick={handleSave} disabled={loading} className="p-3 bg-primary text-white rounded-full" style={{ cursor: loading ? 'not-allowed' : 'pointer' }}>
+          Save
+        </button>
+      </div>
+      {/* Loading indicator below input field and save button */}
+      <div className="text-white mt-2">
+        {loading && <span>Loading...</span>}
+      </div>
+      <div className="text-white">
+        <p>{output}</p>
       </div>
     </div>
   );
 };
 
 export default Timeline;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
